af17529b626e481a7e524a67f787cfa9ee62ac72
Forz70043 <alfonso.pisicchio@libero.it>
2021-04-20 20:59:48 +0200

    remove pack from app

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const { TypewriterClass } = require("typewriter-effect");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
