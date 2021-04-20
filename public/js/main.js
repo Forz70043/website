6171b769f82e8682b8dba11fb749d4c2ac0f53ed
Forz70043 <alfonso.pisicchio@libero.it>
2021-04-20 20:43:53 +0200

    js client side

---
+++ public/js/main.js
@@ -0,0 +1,25 @@
+
+
+var el = document.getElementById('whoIam');
+console.log(el);
+/* 
+var typewriter = new Typewriter(el, {
+    loop: true,
+    delay: 75
+});
+
+
+typewriter
+    .typeString('Programmatore')
+    .pause(1000)
+    .deleteAll()
+    .typeString('Developer')
+    .pause(1000)
+    .deleteAll()
+    .typeString('Backend Developer')
+    .pause(1000)
+    .deleteAll()
+    .typeString('Sviluppatore')
+    .pauseFor(1000)
+    .start();
+     */
